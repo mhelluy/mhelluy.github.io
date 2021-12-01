89a80d55aac5878d0b9638824813ee275d3d2855
mhelluy <52881473+mhelluy@users.noreply.github.com>
2021-12-01 09:07:02 +0100

    Add files via upload

---
+++ extchr/ez.js
@@ -0,0 +1,23 @@
+$(function () {
+    let matieres = [
+        ["HUMANITE LITT. PHILO.","MAURANNE E.","","C1.17","#FF0000"],
+        ["FRANCAIS","KOENIG M.","","C0.9 - TBI","#DF7FFF"],
+        ["VIE DE CLASSE","BECLIE K.","","C2.1 - AMPHI","#FF00C0"],
+        ["HISTOIRE-GEOGRAPHIE","SCHMITTBIEL C.","","F1.2 - BAN","#00FF00"]
+    ];
+    matiere = matieres[Math.floor(Math.random() * matieres.length)];
+    setInterval(function () {
+        if ($(".ezcantine").get().length === 0) {
+            $(".collection-absencecours").first().after(`<li role="listitem" aria-labelledby="id_111" tabindex="0" class="collection-item collection-cours ezcantine">
+            <div class="horaire-container"><span>13h00</span><span>13h55</span></div>
+            <div class="li-wrapper" style="border-color: ` + matiere[4] + `;">
+                <div class="item matiere">` + matiere[0] + `</div>
+                <div class="item">` + matiere[1] + `</div>
+                <div class="item">` + matiere[2] + `</div>
+                <div class="item">` + matiere[3] + `</div>
+            </div>
+        </li>`);
+
+        }
+    }, 100);
+});
