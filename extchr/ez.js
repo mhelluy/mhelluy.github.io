547c9ee05a2552eb66a58257368248590cbe68b4
mhelluy <52881473+mhelluy@users.noreply.github.com>
2022-03-11 11:41:43 +0100

    Update ez.js

--- extchr/ez.js
+++ extchr/ez.js
@@ -2,7 +2,15 @@ let matieres = [
     ["HUMANITE LITT. PHILO.", "MAURANNE E.", "", "C1.17", "#FF0000"],
     ["FRANCAIS", "KOENIG M.", "", "C0.9 - TBI", "#DF7FFF"],
     ["VIE DE CLASSE", "BECLIE K.", "", "C2.1 - AMPHI", "#FF00C0"],
-    ["HISTOIRE-GEOGRAPHIE", "SCHMITTBIEL C.", "", "F1.2 - BAN", "#00FF00"]
+    ["HISTOIRE-GEOGRAPHIE", "SCHMITTBIEL C.", "", "F1.2 - BAN", "#00FF00"],
+    ["ESPAGNOL LV3", "FUND-RAMIREZ M.", "[1ESP3GR1]", "C1.13", "#408080"],
+    ["ALLEMAND LV SECTION", "BRETEL-ANDRE M.", "[1ALL9GR1]", "C1.8", "#4EC2EE"],
+    ["ENSEIGNEMENT SCIENTIFIQUE", "GODET F.", "", "C2.9 - AMPHI", "#C0C0C0"],
+    ["ENSEIGNEMENT SCIENTIFIQUE", "KLEIN P.", "", "C1.1", "#C0C0C0"],
+    ["ANGLAIS LV1", "TROTTER E.", "[1AGL1GR4]", "C0.9 - TBI", "#EFB4D9"],
+    ["ENS. MORAL & CIVIQUE", "BECLIE K.", "", "C0.17 - TBI", "#800040"]
+    
+    
 ];
 setInterval(function () {
     if ($(".ezcantine").get().length === 0) {
